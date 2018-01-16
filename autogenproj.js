5889eccc27ad03597055c28b5ee53920ab3beeb1
RGM-89sc <171716669@qq.com>
2018-01-16 15:35:41 +0800

    0.2

--- autogenproj.js
+++ autogenproj.js
@@ -6,11 +6,11 @@ let projectPath;
 /**
  * 判断目录路径的合法性
  *
- * @param {string} dirName 需要进行合法性判断的文件名
  * @param {string} path 需要进行合法性判断的路径
+ * @param {string} dirName 需要进行合法性判断的文件名
  * @return {string} 合法则返回目录的路径
  */
-function dirPathJudgment(dirName, path) {
+function dirPathJudgment(path, dirName) {
     if (!/^.+\/$/.test(path)) path += '/';
     let tmp = path + dirName + '/',
         regex = /^(?:[a-zA-Z]:)?\/(?:[^\\?\/*|<>:"]+\/)+$/;
@@ -56,14 +56,36 @@ function fsExistsSync(path) {
 }
 
 /**
+ * 在dirName未定义的情况下自动获取文件夹名
+ *
+ * @param {string} path 目录路径
+ * @return {string} 返回自动获取的文件夹名
+ */
+function getDirName(path) {
+    let files = fs.readdirSync(path),
+        re = /^Project([\d]+)$/,
+        num = [];
+    files.forEach((value) => {
+        if (re.test(value)) num.push(parseInt(value.match(re)[1]));
+    });
+    if (num.length === 0) return 'Project1';
+    let max = num[0];
+    for (let i = 1; i < num.length; i++) {
+        if (num[i] > max) max = num[i];
+    }
+    return 'Project' + (max + 1);
+}
+
+/**
  * 新建项目目录 & 生成目录初始文件
  *
- * @param {string} dirName 项目目录名
  * @param {string} path 项目父目录路径
+ * @param {string} dirName 项目目录名
  * @return {number} 返回-1表示发生错误，返回0表示执行成功
  */
-function mkProjectDir(dirName, path) {
-    projectPath = dirPathJudgment(dirName, path);
+function mkProjectDir(path, dirName) {
+    if (!dirName) dirName = getDirName(path);
+    projectPath = dirPathJudgment(path, dirName);
     if (projectPath === '') {
         console.log('路径格式错误');
         return -1;
@@ -98,7 +120,7 @@ function mkProjectDir(dirName, path) {
  */
 function mkForkDir(dirName, genFile, fileName) {
     if (projectPath) {
-        let path = dirPathJudgment(dirName, projectPath);
+        let path = dirPathJudgment(projectPath, dirName);
         if (path === '') {
             console.log('路径格式错误');
             return -1;
@@ -158,7 +180,14 @@ exports.openEditor = (editorName, path) => {
     return 0;
 };
 
-function tasks(dirName, path) {
+/**
+ * 执行
+ *
+ * @param {string} path 项目父目录路径
+ * @param {string} dirName 项目目录名
+ * @return {number} 返回1表示发送错误，返回0表示执行成功
+ */
+function tasks(path, dirName) {
     function success(val) {
         for (let i = 0; i < val.length; i++) {
             if (val[i] === -1) {
@@ -172,7 +201,7 @@ function tasks(dirName, path) {
     if (!arguments[3]) arguments[3] = 'style';
     if (!arguments[4]) arguments[4] = 'main';
     let taskList = [
-        mkProjectDir(dirName, path),
+        mkProjectDir(path, dirName),
         mkForkDir('css', arguments[2], arguments[3]),
         mkForkDir('js', arguments[2], arguments[4]),
         mkForkDir('img')
@@ -183,26 +212,24 @@ function tasks(dirName, path) {
 /**
  * 默认方式生成项目
  *
- * @param {string} dirName 项目目录名
  * @param {string} path 项目父目录路径
- * @return {number} 返回-1表示发生错误，返回0表示执行成功
+ * @param {string} dirName 项目目录名
  */
-exports.defaultGenerateProject = (dirName, path) => {
-    if (tasks(dirName, path) === 0) console.log('项目生成完毕');
+exports.defaultGenerateProject = (path, dirName) => {
+    if (tasks(path, dirName) === 0) console.log('项目生成完毕');
     else console.log('项目生成失败');
 };
 
 /**
  * 默认方式生成项目并打开编辑器
  *
- * @param {string} dirName 项目目录名
  * @param {string} path 项目父目录路径
+ * @param {string} dirName 项目目录名
  * @param {string} editorName 编辑器名称
  * @param {string} editorPath 编辑器路径
- * @return {number} 返回-1表示发生错误，返回0表示执行成功
  */
-exports.defaultGenerateProjectOpenEditor = (dirName, path, editorName, editorPath) => {
-    if (tasks(dirName, path) === 0) console.log('项目生成完毕');
+exports.defaultGenerateProjectOpenEditor = (path, dirName, editorName, editorPath) => {
+    if (tasks(path, dirName) === 0) console.log('项目生成完毕');
     else console.log('项目生成失败');
     exports.openEditor(editorName, editorPath);
 };
@@ -210,15 +237,14 @@ exports.defaultGenerateProjectOpenEditor = (dirName, path, editorName, editorPat
 /**
  * 生成项目
  *
- * @param {string} dirName 项目目录名
  * @param {string} path 项目父目录路径
+ * @param {string} dirName 项目目录名
  * @param {boolean} genFile 是否生成初始文件
  * @param {string} CSSFileName CSS初始文件名，仅当genFile为true时有效
  * @param {string} JSFileName JS初始文件名，仅当genFile为true时有效
- * @return {Object} 返回值
  */
-exports.GenerateProject = (dirName, path, genFile, CSSFileName, JSFileName) => {
-    if (tasks(dirName, path, genFile, CSSFileName, JSFileName) === 0)
+exports.GenerateProject = (path, dirName, genFile, CSSFileName, JSFileName) => {
+    if (tasks(path, dirName, genFile, CSSFileName, JSFileName) === 0)
         console.log('项目生成完毕');
     else console.log('项目生成失败');
 };
